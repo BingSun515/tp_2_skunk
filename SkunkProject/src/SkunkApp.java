0611f2e693dc6ba1f55008f1046cd3bfda7a4461
Bing Sun <sun04877@stthomas.edu>
2019-03-09 12:27:05 -0600

    commit new SkunkApp

---
+++ SkunkProject/src/SkunkApp.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.introcs.StdOut;
+public class SkunkApp
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		StdOut.println("Welcome to 635 Skunk!");
+	}
+
+}
