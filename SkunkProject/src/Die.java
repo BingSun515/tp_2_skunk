9ac9ebff268b1197b337e7dfd247195f26c67263
Murugavel <smurugavel@stthomas.edu>
2019-03-09 18:39:19 -0600

    TP1-First unit test

--- SkunkProject/src/Die.java
+++ SkunkProject/src/Die.java
@@ -2,9 +2,19 @@
 public class Die
 {
 	private int lastRoll;
+	private int[] rolls;
+	private boolean predictable = false;
 
 	public Die()
 	{
+		this.predictable = false;
+		this.roll();
+	}
+	
+	public Die(int[] initialValues)
+	{
+		this.rolls = initialValues;
+		this.predictable = true;
 		this.roll();
 	}
 
@@ -16,7 +26,12 @@ public class Die
 
 	public void roll() // note how this changes Die's state, but doesn't return anything
 	{
-		this.lastRoll = (int) (Math.random() * 6 + 1);
+		if (!predictable)
+			this.lastRoll = (int) (Math.random() * 6 + 1);
+		else
+		{
+			this.lastRoll = this.rolls[0];
+		}
 	}
 	
 	@Override
---
+++ SkunkProject/test/TestDie.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestDie {
+
+	@Test
+	public void Die_NewInstance_Rolls_a_die() {
+		Die die = new Die(new int[] {5});
+		
+		assertEquals(5, die.getLastRoll());
+	}
+}
