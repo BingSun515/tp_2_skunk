2e4d1d566b91527f091ded7197eb88685fab5a1e
Bing Sun <sun04877@stthomas.edu>
2019-03-11 20:57:18 -0500

    TestDie

--- SkunkProject/src/Die.java
+++ SkunkProject/src/Die.java
@@ -2,20 +2,25 @@
 public class Die
 {
 	private int lastRoll;
-	private int[] rolls;
 	private boolean predictable = false;
-
+	private int[] rolls;
+	private int index_of_next_roll;
+	
 	public Die()
 	{
-		this.predictable = false;
 		this.roll();
 	}
-	
-	public Die(int[] initialValues)
+
+	public Die(int[] predictable_rolls)
 	{
-		this.rolls = initialValues;
+		if(predictable_rolls == null)
+		{
+			throw new RuntimeException("null initializing int[] array");
+		}
+		
 		this.predictable = true;
-		this.roll();
+		this.rolls = predictable_rolls;
+		this.index_of_next_roll = 0;
 	}
 
 	public int getLastRoll() // getter or accessor method
@@ -24,16 +29,22 @@ public class Die
 		return this.lastRoll;
 	}
 
-	public void roll() // note how this changes Die's state, but doesn't return anything
+	public void roll() // note how this changes Die's state, but doesn't return
+						// anything
 	{
 		if (!predictable)
 			this.lastRoll = (int) (Math.random() * 6 + 1);
 		else
 		{
-			this.lastRoll = this.rolls[0];
+			this.lastRoll = this.rolls[index_of_next_roll];
+			index_of_next_roll++;
+			if (index_of_next_roll >= this.rolls.length)
+			{
+				index_of_next_roll = 0; //back to start
+			}
 		}
 	}
-	
+
 	@Override
 	public String toString() // this OVERRIDES the default Object.toString()
 	{
--- SkunkProject/test/TestDie.java
+++ SkunkProject/test/TestDie.java
@@ -1,13 +1,78 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class TestDie {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDie
+{
+	private Die die;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		int[] init_values = new int[] { 3, 2, 1 };
+		die = new Die(init_values);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
 
 	@Test
-	public void Die_NewInstance_Rolls_a_die()
+	public void test_initialization_of_predictable_die()
 	{
-		Die die = new Die(new int[] {5});
-		
-		assertEquals(5, die.getLastRoll());
+		die.roll();
+		int value = die.getLastRoll();
+
+		assertEquals("first value not 3", 3, value);
+	}
+
+	@Test
+	public void test_roll_2_of_predictable_die()
+	{
+		die.roll();
+		assertEquals("first value not 3", 3, die.getLastRoll());
+		die.roll();
+		assertEquals("second value not 2", 2, die.getLastRoll());
+
+	}
+
+	@Test
+	public void test_roll_3_of_predictable_die()
+	{
+		die.roll();
+		die.roll();
+		die.roll();
+		assertEquals("third value not 1", 1, die.getLastRoll());
+	}
+
+	@Test
+	public void test_roll_4_of_predictable_die_with3_rolls()
+	{
+		die.roll();
+		die.roll();
+		die.roll();
+		die.roll();
+		assertEquals("fourth value not wrapping back to first value 3", 3, die.getLastRoll());
+	}
+	
+	@Test
+	public void test_roll_5_of_predictable_die_with3_rolls()
+	{
+		die.roll();
+		die.roll();
+		die.roll();
+		die.roll();
+		die.roll();
+		assertEquals("fourth value not wrapping back to first value 3", 2, die.getLastRoll());
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void test_null_initial_int_array()
+	{
+		Die die = new Die(null);
+		die.roll();
 	}
 }
