257733dee7d764379baa417a01490d8a01f9620f
Murugavel <smurugavel@stthomas.edu>
2019-03-12 20:31:07 -0500

    TP1-Test Dice, exception tests

--- SkunkProject/test/TestDice.java
+++ SkunkProject/test/TestDice.java
@@ -16,7 +16,7 @@ public class TestDice {
 	}
 	
 	@Test
-	public void Dice_roll_once_twonumbers_equals_lastRoll_total()
+	public void Dice_roll_twice_twonumbers_equals_lastRoll_total()
 	{
 		Die die1 = new Die(new int[] {3, 4});
 		Die die2 = new Die(new int[] {1, 2});
@@ -24,11 +24,29 @@ public class TestDice {
 		Dice dice = new Dice(die1, die2);
 		
 		dice.roll();
-		
 		assertEquals("value is not 4", 4, dice.getLastRoll());
 		
 		dice.roll();
-		
 		assertEquals("value is not 6", 6, dice.getLastRoll());
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void Dice_roll_once_OneEmptyArray_throws_ArrayIndexOutOfBoundsException()
+	{
+		Die die1 = new Die(new int[] {3, 4});
+		Die die2 = new Die(new int[] {});
+		
+		Dice dice = new Dice(die1, die2);		
+		dice.roll();
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void Dice_roll_once_OneEmptyArray_throws_RuntimeException()
+	{
+		Die die1 = new Die(new int[] {3, 4});
+		Die die2 = new Die(null);
+		
+		Dice dice = new Dice(die1, die2);		
+		dice.roll();
 	}
 }
