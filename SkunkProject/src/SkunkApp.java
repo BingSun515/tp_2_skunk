af8bbcdd6daf1a1138d6722666de94bd22226498
Hsiang Teng <Hsiang Teng@10.50.83.44>
2019-03-15 18:34:03 -0500

    add some function in SkunkApp

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -4,9 +4,17 @@ public class SkunkApp
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
-		StdOut.println("Welcome to 635 Skunk!");
-	}
-
+		StdOut.println("Thanks for playing our Skunk game. This game is under develop.");
+		StdOut.println("Setting Die sequence. Die1 = 6,5,4 AND Die2 = 3,2,1.");
+		
+		Die die1 = new Die(new int[] {6,5,4});
+		Die die2 = new Die(new int[] {3,2,1});
+		Dice dice = new Dice(die1, die2);
+		dice.roll();
+		
+		StdOut.println("die1 roll: " + die1.getLastRoll());
+		StdOut.println("die2 roll: " + die2.getLastRoll());
+		StdOut.println("dice roll: " + dice.getLastRoll());
+		
+	}	
 }
