0909f4baccb7706dc2eb797da5a62f1eeab007cb
Hsiang Teng <Hsiang Teng@10.50.83.44>
2019-04-03 21:45:31 -0500

    TestPlayer

--- SkunkProject/test/TestPlayer.java
+++ SkunkProject/test/TestPlayer.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 public class TestPlayer {
 
 	@Test
-	public void PlayerTest() {
+	public void PlayerTest() 
+	{
 		Player p1 = new Player("Eric");
 		assertEquals(p1.getPlayerName(), "Eric");
+	}
+	
+	@Test
+	public void PlayerActiveTest() 
+	{
+		Player p1 = new Player("Eric");
+		p1.setActivePlayer();
+		assertEquals(p1.isPlayerActive(), true);
 	}
 
 }
