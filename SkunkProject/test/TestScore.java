65a42425d7ec4eaae125e25e8565801171eac75f
Hsiang Teng <Hsiang Teng@192.168.86.41>
2019-04-07 13:37:13 -0500

    format

--- SkunkProject/test/TestScore.java
+++ SkunkProject/test/TestScore.java
@@ -7,32 +7,32 @@ public class TestScore {
 	@Test
 	public void Score() {
 		Score s1 = new Score();
-		assertEquals(s1.getTurnScore(), 0);
-		assertEquals(s1.getChipChangeScore(), 0);
-		assertEquals(s1.getKittyChangeScore(), 0);
+		assertEquals(0, s1.getTurnScore());
+		assertEquals(0, s1.getChipChangeScore());
+		assertEquals(0, s1.getKittyChangeScore());
 	}
 
 	@Test
 	public void ScoreInt() {
 		Score s1 = new Score(100);
-		assertEquals(s1.getTurnScore(), 100);
+		assertEquals(100, s1.getTurnScore());
 	}
 	
 	@Test
 	public void ScorewithThreeInt() {
 		Score s1 = new Score(50,100,150);
-		assertEquals(s1.getTurnScore(), 50);
-		assertEquals(s1.getChipChangeScore(), 100);
-		assertEquals(s1.getKittyChangeScore(), 150);
+		assertEquals(50, s1.getTurnScore());
+		assertEquals(100, s1.getChipChangeScore());
+		assertEquals(150, s1.getKittyChangeScore());
 	}
 	
 	@Test
 	public void setTurnScore() {
 		Score s1 = new Score(50,100,150);
 		s1.setTurnScore(1, 2, 3);
-		assertEquals(s1.getTurnScore(), 51);
-		assertEquals(s1.getChipChangeScore(), 102);
-		assertEquals(s1.getKittyChangeScore(), 153);
+		assertEquals(51, s1.getTurnScore());
+		assertEquals(102, s1.getChipChangeScore());
+		assertEquals(153, s1.getKittyChangeScore());
 	}
 
 }
