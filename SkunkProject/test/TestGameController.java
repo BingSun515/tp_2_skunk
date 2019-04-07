5a3c10861e7f443f76850bf01620e40b13d1fdd2
Hsiang Teng <Hsiang Teng@192.168.86.41>
2019-04-07 14:20:10 -0500

    Partial Testing

---
+++ SkunkProject/test/TestGameController.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGameController {
+
+	@Test
+	public void gcPlayer() {
+		GameController gc1 = new GameController();
+		gc1.setPlayer("Eric");
+		assertEquals("Eric", gc1.getPlayerName());
+	}
+	
+	@Test
+	public void gcPlayer2() {
+		GameController gc1 = new GameController();
+		gc1.setPlayer(null);
+		assertEquals(null, gc1.getPlayerName());
+	}
+	
+	@Test
+	public void gcStartGame() {
+		GameController gc1 = new GameController();
+		gc1.startGame("Eric");
+	}
+}
---
+++ SkunkProject/test/TestTurn.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+
+import org.junit.Test;
+
+public class TestTurn {	
+	
+	@Test
+	public void setLastRoll0() {
+		Turn t1 = new Turn();
+	}
+
+}
