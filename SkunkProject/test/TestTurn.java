ec6833ef8098b8e4021750258d8ad8aad5affb64
Hsiang Teng <Hsiang Teng@10.50.83.44>
2019-04-09 18:54:53 -0500

    add some turnTest

--- SkunkProject/test/TestTurn.java
+++ SkunkProject/test/TestTurn.java
@@ -8,8 +8,26 @@ import org.junit.Test;
 public class TestTurn {	
 	
 	@Test
-	public void setLastRoll0() {
+	public void setLastRoll() {
 		Turn t1 = new Turn();
+		Roll lastRoll = null;
+		t1.setLastRoll(lastRoll);
 	}
+	
+	@Test
+	public void getLastRoll() {
+		Turn t1 = new Turn();
+		Roll lastRoll = null;
+		t1.setLastRoll(lastRoll);
+		t1.getLastRoll();
+	}
+
+	@Test
+	public void rollAndSetScore() {
+		Turn t1 = new Turn();
+		t1.rollAndSetScore();
+	}
+	
+
 
 }
