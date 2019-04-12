3186deaeba84df48672add44124a648191fc8c53
Hsiang Teng <Hsiang Teng@10.50.83.44>
2019-04-11 19:14:45 -0500

    gamecontroller need to fix it

--- SkunkProject/test/TestGameController.java
+++ SkunkProject/test/TestGameController.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 public class TestGameController {
 
 	@Test
-	public void gcPlayer() {
+	public void setPlayer1() {
 		GameController gc1 = new GameController();
 		gc1.setPlayer("Eric");
 		assertEquals("Eric", gc1.getPlayerName());
 	}
 	
 	@Test
-	public void gcPlayer2() {
+	public void setPlayer2() {
 		GameController gc1 = new GameController();
 		gc1.setPlayer(null);
 		assertEquals(null, gc1.getPlayerName());
@@ -22,6 +22,7 @@ public class TestGameController {
 //	public void gcStartGame() {
 //		GameController gc1 = new GameController();
 //		gc1.startGame("Eric");
-//		assertEquals("Eric", gc1.getPlayerName());
+//		
 //	}
+		
 }
