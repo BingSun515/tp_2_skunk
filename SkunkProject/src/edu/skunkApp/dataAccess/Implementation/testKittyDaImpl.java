a9278d3a5a2566c005554e37de14477b6833ae8c
Hsiang Teng <Hsiang Teng@192.168.86.41>
2019-05-08 11:57:30 -0500

    testing mockito

--- SkunkProject/src/edu/skunkApp/dataAccess/Implementation/testKittyDaImpl.java
+++ SkunkProject/src/edu/skunkApp/dataAccess/Implementation/testKittyDaImpl.java
@@ -1,22 +1,11 @@
 package edu.skunkApp.dataAccess.Implementation;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 public class testKittyDaImpl {
 	
-	private ArrayList<Integer> _chips;
-	
-	@Test (expected=NullPointerException.class)
-	  public void testKittyDaImpl() {
-		_chips = new ArrayList<Integer>();
-		_chips.add(1);
-		_chips.add(2);
-		_chips.add(3);
-		System.out.println(_chips);
-		KittyDaImpl KD1 = new KittyDaImpl();
-	  }
+	@Test
+	public void testKittyDaImpl() {
+		//KittyDaImpl KDtest = Mockito.mock(KittyDaImpl.class);
+	}
 }
