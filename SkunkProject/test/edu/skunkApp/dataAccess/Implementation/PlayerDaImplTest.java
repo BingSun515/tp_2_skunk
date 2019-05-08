ea4c5835101cb1603973eb34027599f7c13108ca
Hsiang Teng <Hsiang Teng@192.168.86.41>
2019-05-08 16:31:21 -0500

    starting Player and Roll

---
+++ SkunkProject/test/edu/skunkApp/dataAccess/Implementation/PlayerDaImplTest.java
@@ -0,0 +1,18 @@
+package edu.skunkApp.dataAccess.Implementation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import edu.skunkApp.data.Player;
+
+public class PlayerDaImplTest {
+	
+	@Test
+	public void testPlayerDaImpl() {
+		
+	}
+
+}
---
+++ SkunkProject/test/edu/skunkApp/dataAccess/Implementation/RollScoreDaImplTest.java
@@ -0,0 +1,12 @@
+package edu.skunkApp.dataAccess.Implementation;
+
+import org.junit.Test;
+
+public class RollScoreDaImplTest {
+
+	@Test
+	public void testRollScoreDaImpl() {
+		RollScoreDaImpl rsd1 = new RollScoreDaImpl();
+		
+	}
+}
