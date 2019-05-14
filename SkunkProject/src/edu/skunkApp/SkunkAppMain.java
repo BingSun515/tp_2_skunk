055568f48e6d76dc63391cc5f56c150e152c25fe
Murugavel <smurugavel@stthomas.edu>
2019-05-14 17:31:04 -0500

    add juniper

---
+++ SkunkProject/src/edu/skunkApp/SkunkAppMain.java
@@ -0,0 +1,18 @@
+package edu.skunkApp;
+
+import edu.skunkApp.controller.GameController;
+
+public class SkunkAppMain {
+
+	public static void main(String[] args)
+	{
+		GameController gameController = new GameController();
+		gameController.StartGame();
+//		System.out.println("Hello world !!!");
+//		StdOut.println("Hello World");
+//		gameUi.startGame();
+		
+//		SkunkAppUI ui = new SkunkAppUI();
+//		SkunkAppMain.getUserInput();
+	}
+}
