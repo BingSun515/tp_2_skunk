44b021d34a614f96b72d91c95201f5065b7e81c0
Murugavel <smurugavel@stthomas.edu>
2019-05-14 19:09:54 -0500

    add jdk 11 to pom

--- SkunkProject/test/edu/skunkApp/businessobject/Implementation/RollBoImplTest.java
+++ SkunkProject/test/edu/skunkApp/businessobject/Implementation/RollBoImplTest.java
@@ -1,10 +1,7 @@
 package edu.skunkApp.businessobject.Implementation;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
-import edu.skunkApp.domainModels.RollDm;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RollBoImplTest {
 	
