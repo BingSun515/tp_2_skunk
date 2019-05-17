697b17a6bb4bacd89b8a0a9a2d43321ce9aa7496
Hsiang Teng <Hsiang Teng@10.50.83.44>
2019-05-17 16:09:35 -0500

    33%

--- SkunkProject/test/edu/skunkApp/businessobject/Implementation/RollScoreBoImplTest.java
+++ SkunkProject/test/edu/skunkApp/businessobject/Implementation/RollScoreBoImplTest.java
@@ -1,5 +1,18 @@
 package edu.skunkApp.businessobject.Implementation;
 
+import org.junit.jupiter.api.Test;
+
+import edu.skunkApp.common.SkunkEnum;
+import edu.skunkApp.domainModels.RollScoreDm;
+
 public class RollScoreBoImplTest {
+	
+	@Test
+	public void testresetPlayerScoresForSkunk() {
+		RollScoreBoImpl rsbi1= new RollScoreBoImpl();
+		RollScoreDm rsd1 = new RollScoreDm();
+		rsd1.rollStatus = SkunkEnum.SINGLESKUNK;
+		rsbi1.resetPlayerScoresForSkunk(rsd1);
+	}
 
 }
